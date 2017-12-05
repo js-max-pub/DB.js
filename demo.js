f64888d8fcdce86aa25d2ccaf38ef684a8bf084e
Max Fechner <max.fechner@gmail.com>
2017-12-05 16:05:44 +0100

    ...

--- demo.js
+++ demo.js
@@ -14,13 +14,20 @@ if (!LOG) LOG = console.log;
 // var db = new DB('db');
 // db.addStore('contacts', ['name', '!email','city']);
 
-db = new DB('ContactApp', 1, {
-	contacts: '@email,name,city,!phone'
+db = new DB('ContactApp', 3, {
+	contacts: '@email,name,city,phone'
 });
 
-db = new DB('ContactApp');
-db.addStore('contacts', ['name', '!email', 'city']);
-db.addStore('contacts', '@email, name, city, !phone');
+db.upgrade((currentVersion) => {
+	switch (currentVersion) {
+		case 1: // balsdf
+		case 2: // fasdfasd
+	}
+});
+
+// db = new DB('ContactApp');
+// db.addStore('contacts', ['name', '!email', 'city']);
+// db.addStore('contacts', '@email, name, city, !phone');
 
 
 db.contacts.add({
@@ -31,7 +38,7 @@ db.contacts.add({
 	city: 'Hamburg'
 });
 
-db.contacts.add('http://example.com/addressbook.json');
+// db.contacts.add('http://example.com/addressbook.json');
 
 db.contacts.query('name', ['a', 'o']).forEach(contact => {
 	console.log('contact', contact);
